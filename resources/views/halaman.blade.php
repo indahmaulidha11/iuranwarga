787944e49eae8dbb849333cd609cb28b2706eea2
puput55 <sabiputri784@gmail.com>
2025-08-06 07:42:32 +0700

    ubah

--- resources/views/halaman.blade.php
+++ resources/views/halaman.blade.php
@@ -77,7 +77,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNav" style="text-decoration-color: #fce4ec">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">About</a></li>
