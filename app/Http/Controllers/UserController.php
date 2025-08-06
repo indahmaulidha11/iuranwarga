7a75f630b6fa1ca33cbfcd31522fc997f1b4772f
indahmaulidha11 <indahmaulidha11@gmail.com>
2025-08-06 11:21:19 +0700

    perubahan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -111,7 +111,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('users.index', compact('users'));
+        return view('admin.users', compact('users'));
     }
 
     /**
---
+++ resources/views/admin/create-users.blade.php
@@ -0,0 +1,47 @@
+@extends('template')
+
+@section('title', 'Tambah User')
+
+@section('content')
+<div class="container mt-5">
+    <h3>Tambah User Baru</h3>
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf
+
+        <div class="mb-3">
+            <label>Nama Lengkap</label>
+            <input type="text" name="name" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label>Password</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label>No HP</label>
+            <input type="text" name="nohp" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label>Alamat</label>
+            <textarea name="address" class="form-control" required></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label>Level</label>
+            <select name="level" class="form-control" required>
+                <option value="warga">Warga</option>
+                <option value="admin">Admin</option>
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+</div>
+@endsection
