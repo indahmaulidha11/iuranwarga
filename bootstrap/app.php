00166307795b8d3cd32be28db9470991f2b87b0f
puput55 <sabiputri784@gmail.com>
2025-08-06 09:31:50 +0700

    p

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AuthAdmin;
+use App\Http\Middleware\AuthWarga;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->appendToGroup('admin', [
+            AuthAdmin::class,
+        ]);
+        $middleware->appendToGroup('warga', [
+            AuthWarga::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
